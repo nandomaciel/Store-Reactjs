5af3c65ab5d734d9d0b93a16d51110bbd120f969
Fernando Rodrigues Maciel <fernandomaciel@ufrn.edu.br>
2020-07-30 23:40:20 -0300

    Updates

---
+++ precache-manifest.e55c7edf6f80839c9fc652938b3d350a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072e912ba744a57ec036c5ff56cd70ad",
+    "url": "/Store-Reactjs/index.html"
+  },
+  {
+    "revision": "2c38c344abdf74584565",
+    "url": "/Store-Reactjs/static/css/main.7eb1fce0.chunk.css"
+  },
+  {
+    "revision": "a47e49a16fd747bfe501",
+    "url": "/Store-Reactjs/static/js/2.ce5112a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Store-Reactjs/static/js/2.ce5112a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c38c344abdf74584565",
+    "url": "/Store-Reactjs/static/js/main.e63cb8c2.chunk.js"
+  },
+  {
+    "revision": "e4eed9c241b7f4da68a3",
+    "url": "/Store-Reactjs/static/js/runtime-main.d859f66c.js"
+  },
+  {
+    "revision": "20d1d7bb569e9158b47473eba9c4ca2c",
+    "url": "/Store-Reactjs/static/media/contato.20d1d7bb.jpg"
+  }
+]);
