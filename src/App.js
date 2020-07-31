24fa74525038e2846c0f50d321fe51e8f2f30eff
Fernando Rodrigues Maciel <fernandomaciel@ufrn.edu.br>
2020-07-31 02:00:46 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/Store-Reactjs" element={<Produtos />} />
-            <Route path="/produto/:id" element={<Produto />} />
+            <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
             <Route path="/contato" element={<Contato />} />
           </Routes>
         </div>
