b1996077f4595ad59d3dff5be01ad61684e5a473
Fernando Rodrigues Maciel <fernandomaciel@ufrn.edu.br>
2020-07-31 02:08:39 -0300

    rotas

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
           <NavLink 
             activeClassName={styles.active} 
             className={styles.link} 
-            to="/Store-Reactjs" 
+            to="/" 
             end
           >
             Produtos
@@ -20,7 +20,7 @@ const Header = () => {
           <NavLink 
             activeClassName={styles.active} 
             className={styles.link} 
-            to="/Store-Reactjs/contato"
+            to="contato"
           >
             Contato
           </NavLink>
